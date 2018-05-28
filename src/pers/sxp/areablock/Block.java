48a3d8d75d8794ad333080e8c6dc5a44402d3d77
Bryce <206037736@qq.com>
2018-05-28 21:10:27 +0800

    实现了一个简单的区块链

--- src/pers/sxp/areablock/Block.java
+++ src/pers/sxp/areablock/Block.java
@@ -9,15 +9,65 @@ import java.util.Date;
  * 时间:2018年4月15日下午8:16:09
  */
 public class Block {
+	/**
+	 * 本区块的的hash
+	 */
 	public String hash;
-	public String previousHash;
-	private String data;	//our data will be a simple message
-	private long timeStamp;	//as number of milliseconds since
 	
-	//Block Constructor
+	/**
+	 * 前一个区块的hash值
+	 */
+	public String previousHash;	
+	
+	/**
+	 * 数据是一个简单的消息
+	 */
+	private String data;
+	
+	/**
+	 * 当前时间戳
+	 */
+	private long timeStamp;
+	/**
+	 * 工作量
+	 */
+    private int nonce;
+	
+	/**
+	 * Block Constructor
+	 * @param data
+	 * @param previousHash
+	 */
 	public Block(String data, String previousHash){
 		this.data = data;
 		this.previousHash = previousHash;
-		this.timeStamp = new Date().getTime();
+		this.timeStamp = System.currentTimeMillis();
+		this.hash = calculateHash();	//写入我们计算出来的hash值
+	}
+	
+	/**
+	 * 计算hash值，我们计算的hash值应该包括区块中
+	 * 所有我们不希望被恶意篡改的数据
+	 * @return
+	 */
+	public String calculateHash(){
+		String calculatedhash = StringUtil.applySha256(
+				previousHash + 
+				Integer.toString(nonce) +
+				Long.toString(timeStamp) + data);
+		return calculatedhash;
+	}
+	
+	/**
+	 * 获取挖矿工作量
+	 * @param difficulty 难度
+	 */
+	public void mineBlock(int difficulty){
+		String target = new String(new char[difficulty]).replace('\0', '0');
+		while (!hash.substring(0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("区块开采:" + hash);
 	}
 }
